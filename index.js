3555fa564ac661b3b049eea02f27ae17d7ff166e
Breanne <BreMarotta@gmail.com>
2021-11-08 10:11:21 -0500

    commit tree 22d4778f9b95275ff2edd8951c0ef99b002e4488

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+//Will loop through array
+//Find any time the result: "W"
+//Return year when result: "W"
+//Will return undefined if no "W"
 
+//function superbowlWin(array){
+   // const win = array.find(object => object.result === "W");
+  //  return win;
+//}
+
+function superbowlWin(array){
+    const win = array.find(object => object.result === "W");
+    if (win){
+        return win.year; 
+    } else {
+        return undefined;
+    }
+}
+
+
+//function isW(object){
+   // return object.result === "W";
+//}
